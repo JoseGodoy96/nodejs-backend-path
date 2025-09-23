5c245431347c8a93a408f50b1aa064c44e30ed33
Jose Maria <josemariagodoy1996@gmail.com>
2025-09-23 12:41:39 +0200

    Exercise 3º dia 6 - creacion ejercicio 4º dia 6

--- cli-node/fundamentals/day-6-reviw-miniproject/Mini-exercise/3-exercise.mjs
+++ cli-node/fundamentals/day-6-reviw-miniproject/Mini-exercise/3-exercise.mjs
@@ -1,5 +1,59 @@
-
 /*
-Ejercicio 3 – Async/await
-- Refactoriza un ejercicio de Promises para usar async/await
+Español:
+Crea un programa que simule la obtención de datos de un usuario desde una base de datos y,
+a continuación, la obtención de sus tareas. Implementa las funciones usando Promises y
+encadena las operaciones con .then() y .catch().
+
+English:
+Create a program that simulates fetching user data from a database and then retrieving
+the user’s tasks. Implement the functions using Promises and chain
+the operations with .then() and .catch().
 */
+
+import { resourceLimits } from "worker_threads";
+
+let user1 = {
+	id: 1,
+	name: "antonio",
+	email: "antonio@gmail.com"
+};
+
+let user2 = {
+	id: 2,
+	name: "manolo",
+	email: "manolo@gmail.com"
+};
+
+let user3 = {
+	id: 3,
+	name: "agos",
+	email: "agos@gmail.com"
+};
+
+let users = [user1, user2, user3];
+
+function wait(ms){
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function getUserById(id) {
+	console.log("Loading...");
+	return wait(2000).then(() => {
+		return new Promise((resolve, reject) => {
+			const user = users.find(user => user.id === id);
+			if (user) {
+				resolve(user);
+			} else {
+				reject("Error: no hay usuario con ese id");
+			}
+		});
+	});
+}
+
+getUserById(2)
+	.then(user => {
+		console.log("Usuario encontrado:", user);
+	})
+	.catch(err => {
+		console.error(err);
+	});
---
+++ cli-node/fundamentals/day-6-reviw-miniproject/Mini-exercise/4.exercise.mjs
@@ -0,0 +1,13 @@
+/*
+Español:
+Refactoriza el ejercicio anterior para usar async/await en lugar de .then() y .catch(). 
+Maneja los errores con try...catch y asegúrate de que el flujo de operaciones sea el mismo:
+primero obtener el usuario y después sus tareas.
+
+English:
+Refactor the previous exercise to use async/await instead of .then() and .catch(). 
+Handle errors with try...catch and ensure the flow of operations remains the same: 
+first fetch the user and then their tasks.
+*/
+
+
