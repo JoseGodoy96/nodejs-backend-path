994c072c95761e9fc188532773a7b72ce36270d4
Jose Maria <josemariagodoy1996@gmail.com>
2025-09-25 18:45:59 +0200

    Ejercicio 3 dia 9º

--- cli-node/02-Node-core/Día-9-Sistema-de-Archivos-y-Rutas/Mini-exercise/3-exercise.mjs
+++ cli-node/02-Node-core/Día-9-Sistema-de-Archivos-y-Rutas/Mini-exercise/3-exercise.mjs
@@ -5,3 +5,16 @@ Ejercicio 3 (ES):
 Exercise 3 (EN):
 - Create an async function that reads a file and handles errors if it does not exist.
 */
+
+import { promises as fsp } from "fs";
+
+async function readFileSafe(filePath) {
+	try {
+    	const content = await fsp.readFile(filePath, "utf8");
+    	console.log(content);
+	} catch (error) {
+    	console.error("Error al leer el archivo:", error.message);
+	}
+}
+
+readFileSafe("test.txt");
