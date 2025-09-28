ac3121f1b4fd3418e7e4411a7fdb82e3b26d84af
Jose Maria <josemariagodoy1996@gmail.com>
2025-09-28 23:15:58 +0200

    ejercicio 3 day 12

--- cli-node/02-Node-core/day-12-Introduction-to-express.js/Mini-exercise/3-exercise.mjs
+++ cli-node/02-Node-core/day-12-Introduction-to-express.js/Mini-exercise/3-exercise.mjs
@@ -5,3 +5,24 @@ Ejercicio 3 (ES):
 Exercise 3 (EN):
 - Create a "/time" route that returns the current time in JSON format.
 */
+
+import express from "express";
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Bienvenido a Express"));
+app.get("/json", (req, res) => {
+	res.json({ message: `Hola desde JSON` });
+});
+app.get("/time", (req, res) => {
+	const fecha = new Date();
+	res.json({ time: `${fecha.getHours()}:${fecha.getMinutes()}:${fecha.getSeconds()}` });
+});
+
+app.use((req, res) => res.status(404).send("404 - No encontrada"));
+
+app.listen(PORT, () => {
+	console.log(`Servidor activo en http://localhost:${PORT}`);
+});
+
