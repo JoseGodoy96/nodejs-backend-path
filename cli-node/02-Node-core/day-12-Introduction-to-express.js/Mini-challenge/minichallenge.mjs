29fd785286148a341da51706789b6eb85b779dbb
Jose Maria <josemariagodoy1996@gmail.com>
2025-09-28 23:40:44 +0200

    minichallenge day 12

--- cli-node/02-Node-core/day-12-Introduction-to-express.js/Mini-challenge/minichallenge.mjs
+++ cli-node/02-Node-core/day-12-Introduction-to-express.js/Mini-challenge/minichallenge.mjs
@@ -11,3 +11,29 @@ Mini-Challenge (EN):
 	2. "/users" → return a mock JSON array of users.
 	3. "/status" → return { status: "ok", serverTime: <current time> }.
 */
+
+import express from "express";
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+
+const users = [
+	{ usuario: "1", contraseña: 1234 },
+	{ usuario: "2", contraseña: 1234 },
+	{ usuario: "3", contraseña: 1234 }
+];
+
+app.get("/", (req, res) => res.send("Bienvenido a Express"));
+app.get("/users", (req, res) => {
+	res.json(users);
+});
+app.get("/status", (req, res) => {
+	const fecha = new Date();
+	res.json({ staus: "ok", time: `${fecha.getHours()}:${fecha.getMinutes()}:${fecha.getSeconds()}` });
+});
+
+app.use((req, res) => res.status(404).send("404 - No encontrada"));
+
+app.listen(PORT, () => {
+	console.log(`Servidor activo en http://localhost:${PORT}`);
+});
