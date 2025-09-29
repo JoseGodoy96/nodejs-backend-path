a4fc358bf68be6fc2f68919916ded90e8667cc38
Jose Maria <josemariagodoy1996@gmail.com>
2025-09-29 17:53:02 +0200

    Ejercicio 1 dia 14

--- cli-node/02-Node-core/day-14-ErrorHandling-In-Express/Mini-exercise/2-exercise.mjs
+++ cli-node/02-Node-core/day-14-ErrorHandling-In-Express/Mini-exercise/2-exercise.mjs
@@ -7,3 +7,33 @@ Exercise 2 (EN):
 - Create a "/divide/:a/:b" route that divides a by b.
 - If b is 0 → throw an error "Division by zero not allowed".
 */
+
+import express from "express";
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.status || 500).json({ error: err.message || "Algo salió mal" });
+});
+
+app.get("/", (req, res) => res.send("Servidor con middlewares"));
+app.get("/divide/:a/:b", (req, res, next) => {
+    const a = Number(req.params.a);
+    const b = Number(req.params.b);
+
+    if (b === 0) {
+        const err = new Error("División por cero no permitida");
+        err.status = 400;
+        return next(err);
+    }
+
+    res.json({ result: a / b });
+});
+
+app.use((req, res) => res.status(404).send("404 - No encontrada"));
+
+app.listen(PORT, () => {
+	console.log(`Servidor activo en http://localhost:${PORT}`);
+});
