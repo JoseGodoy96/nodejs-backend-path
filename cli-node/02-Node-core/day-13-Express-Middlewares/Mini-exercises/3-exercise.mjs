cffccdc4f7605f10cee64c7ba755a88618dc041d
Jose Maria <josemariagodoy1996@gmail.com>
2025-09-29 14:53:42 +0200

    exercise 3 dia 13

--- cli-node/02-Node-core/day-13-Express-Middlewares/Mini-exercises/3-exercise.mjs
+++ cli-node/02-Node-core/day-13-Express-Middlewares/Mini-exercises/3-exercise.mjs
@@ -6,3 +6,30 @@ Exercise 3 (EN):
 - Apply a middleware only to the "/admin" route that blocks the request (res.send("Access denied")).
 */
 
+import express from "express";
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+
+const adminBlocker = (req, res, next) => {
+	res.send("Acceso denegado");
+};
+
+app.use((req, res, next) => {
+	console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+	next();
+});
+
+app.get("/", (req, res) => {
+	res.send("Bienvenido al servidor con logger!");
+});
+
+app.get("/about", (req, res) => {
+	res.send("Página acerca de nosotros");
+});
+
+app.use("/admin", adminBlocker);
+
+app.listen(PORT, () => {
+	console.log(`Servidor activo en http://localhost:${PORT}`);
+});
