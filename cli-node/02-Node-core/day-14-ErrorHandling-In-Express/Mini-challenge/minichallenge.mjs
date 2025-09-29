2b3e373c2795c1cd2febebcb9b1f1f969e299744
Jose Maria <josemariagodoy1996@gmail.com>
2025-09-29 18:38:02 +0200

    Minichallenge dia 14

--- cli-node/02-Node-core/day-14-ErrorHandling-In-Express/Mini-challenge/minichallenge.mjs
+++ cli-node/02-Node-core/day-14-ErrorHandling-In-Express/Mini-challenge/minichallenge.mjs
@@ -15,3 +15,45 @@ Mini-Challenge (EN):
 - Global error middleware:
 - Returns { error: message, status: code } in JSON format.
 */
+
+import express from "express";
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+
+const numRandom = (req, res, next) => {
+	const number = Math.random();
+	if (number >= 0.5) {
+		next();
+	} else {
+		const err = new Error("Numero demasiado bajo");
+		err.status = 401;
+		next(err);
+	}
+};
+
+app.get("/", (req, res) => res.send("Servidor con manejo de errores"));
+app.get("/random", numRandom, (req, res) => {
+	res.send("Numero aceptable! Bienvenido!");
+});
+app.get("/user/:id", (req, res) => {
+	const numId = Number(req.params.id);
+	if (isNaN(numId)) {
+		const err = new Error("Id invalido");
+		err.status = 400;
+		return next(err);
+	} else {
+		res.send(`Bienvenido usuario ${numId}`);
+	}
+});
+
+app.use((err, req, res, next) => {
+	console.error(err.message);
+	res.status(err.status || 500).json({ error: err.message, status: err.status });
+});
+
+app.use((req, res) => res.status(404).send("404 - No encontrada"));
+
+app.listen(PORT, () => {
+	console.log(`Servidor activo en http://localhost:${PORT}`);
+});
