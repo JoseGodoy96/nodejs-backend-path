54163c9069b3b815fb03fede58c505156eccdea4
Jose Maria <josemariagodoy1996@gmail.com>
2025-09-29 14:07:44 +0200

    exercise 1 dia 13

--- cli-node/02-Node-core/day-13-Express-Middlewares/Mini-exercises/1-exercise.mjs
+++ cli-node/02-Node-core/day-13-Express-Middlewares/Mini-exercises/1-exercise.mjs
@@ -5,3 +5,18 @@ Ejercicio 1 (ES):
 Exercise 1 (EN):
 - Set up express.json() as a global middleware and create a POST "/echo" route that returns the received JSON.
 */
+
+import express from "express";
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+
+app.post("/echo", (req, res) => {
+    res.json(req.body);
+});
+
+app.use((req, res) => res.status(404).send("404 - No encontrada"));
+
+app.listen(PORT, () => {
+	console.log(`Servidor activo en http://localhost:${PORT}`);
+});
