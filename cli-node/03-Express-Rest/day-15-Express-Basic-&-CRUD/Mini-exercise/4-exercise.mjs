e66798b28713bfea31d9e43bd4bca4322e4a05d8
Jose Maria <josemariagodoy1996@gmail.com>
2025-09-30 10:54:49 +0200

    Ejercicio 4 del dia 15

--- cli-node/03-Express-Rest/day-15-Express-Basic-&-CRUD/Mini-exercise/4-exercise.mjs
+++ cli-node/03-Express-Rest/day-15-Express-Basic-&-CRUD/Mini-exercise/4-exercise.mjs
@@ -4,3 +4,26 @@ Ejercicio 4 (ES):
 Exercise 4 (EN):
 - Create a GET "/users/:id" route that returns the user according to the array index.
 */
+
+import express from "express";
+const app = express();
+const PORT = 3000;
+
+let users = [
+	{ id: 1, name: "Jose", email: "jose@example.com" },
+	{ id: 2, name: "Ana", email: "ana@example.com" },
+	{ id: 3, name: "Luis", email: "luis@example.com" }
+];
+
+app.get("/users/:id", (req, res) => {
+	const { id } = req.params;
+	const user = users.find(u => u.id === Number(id));
+	if (!user) {
+		return res.status(404).json({ error: "Usuario no encontrado" });
+	}
+	res.json(user);
+});
+
+app.listen(PORT, () => {
+	console.log(`Servidor activo en http://localhost:${PORT}`);
+});
