ce5b12bd24d5282d3511f9678dd104767a5c5a25
Jose Maria <josemariagodoy1996@gmail.com>
2025-09-30 10:01:34 +0200

    Ejercicio 2 del dia 15

--- cli-node/03-Express-Rest/day-15-Express-Basic-&-CRUD/Mini-exercise/2-exercise.mjs
+++ cli-node/03-Express-Rest/day-15-Express-Basic-&-CRUD/Mini-exercise/2-exercise.mjs
@@ -5,3 +5,20 @@ Exercise 2 (EN):
 - Create a GET "/users" route that returns a mock array of users in JSON.
 */
 
+import express from "express";
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => res.send("Bienvenido a express"));
+app.get("/users", (req, res) => {
+	const users = [
+		{ id: 1, user: "Jose", password: 1234 },
+		{ id: 2, user: "Ana", password: 5678 },
+		{ id: 3, user: "Luis", password: 9876 }
+	];
+	res.json(users)
+});
+
+app.listen(PORT, () => {
+	console.log(`Servidor activo en http://localhost:${PORT}`);
+});
