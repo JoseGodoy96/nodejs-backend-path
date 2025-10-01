2597b49b51bdd4937ee84bc905c7a665f43bdafd
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-01 13:45:06 +0200

    minichallenge dia 16º

--- cli-node/03-Express-Rest/day-16-Express-Middlewares-&-Validation/Mini-challenge/minichallenge.mjs
+++ cli-node/03-Express-Rest/day-16-Express-Middlewares-&-Validation/Mini-challenge/minichallenge.mjs
@@ -13,3 +13,55 @@ Mini-Challenge (EN):
 	3. POST "/products" route that validates {name, price}.
 	4. Global middleware to catch errors.
 */
+
+import express from "express";
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+	console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+	next();
+});
+
+app.use((req, res, next) => {
+	const start = Date.now();
+	res.on("finish", () => {
+		const time = Date.now() - start;
+		console.log(`[${new Date().toISOString()}] ${req.method} ${req.url} - ${time}ms`);
+	});
+	next();
+})
+
+let products = [];
+
+app.post("/products", (req, res) => {
+	const { name, price } = req.body;
+	if (!name || price === undefined)
+			return res.status(400).send("Falta name o price");
+		const newProduct = { name, price };
+		products.push(newProduct);
+		res.status(201).json(newProduct);
+});
+
+app.get("/", (req, res) => {
+	res.send("Bienvenido a express.js");
+});
+
+app.get("/products", (req, res) => {
+	res.json(products);
+});
+
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(err.status || 500).json({ error: err.message || "Algo salió mal" });
+});
+
+app.use((req, res) => {
+	res.status(404).send("404 - No encontrada");
+});
+
+app.listen(PORT, () => {
+	console.log(`Servidor activo: http://localhost:${PORT}`);
+});
