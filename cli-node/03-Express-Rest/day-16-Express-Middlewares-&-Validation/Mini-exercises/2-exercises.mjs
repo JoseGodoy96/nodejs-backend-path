b80678596f307c93de840e8a3463db58cd510868
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-01 11:17:16 +0200

    ejercicio 2 dia 16º

--- cli-node/03-Express-Rest/day-16-Express-Middlewares-&-Validation/Mini-exercises/2-exercises.mjs
+++ cli-node/03-Express-Rest/day-16-Express-Middlewares-&-Validation/Mini-exercises/2-exercises.mjs
@@ -5,3 +5,27 @@ Exercise 2 (EN):
 - Create a middleware that measures execution time of each request.
 */
 
+import express from "express";
+const app = express();
+const PORT = 3000;
+
+app.use((req, res, next) => {
+	const start = Date.now();
+	res.on("finish", () => {
+		const time = Date.now() - start;
+		console.log(`[${new Date().toISOString()} ${req.method} ${req.url}] - ${time}ms`);
+	})
+	next();
+})
+
+app.get("/", (req, res) => {
+	res.send("Bienvenido a express.js");
+})
+
+app.use((req, res) => {
+	res.status(404).send("404 - No encontrada");
+})
+
+app.listen(PORT, () => {
+	console.log (`Servidor activo en http://localhost:${PORT}`);
+});
