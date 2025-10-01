f44e11dec12d627c1ba24225951b91214171dd62
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-01 13:19:13 +0200

    ejercicio 4 dia 16º

--- cli-node/03-Express-Rest/day-16-Express-Middlewares-&-Validation/Mini-exercises/4-exercises.mjs
+++ cli-node/03-Express-Rest/day-16-Express-Middlewares-&-Validation/Mini-exercises/4-exercises.mjs
@@ -5,3 +5,19 @@ Exercise 4 (EN):
 - Implement a global error handling middleware that returns 500 on error.
 */
 
+import express from "express";
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+	res.send("Bienvenido a express.js");
+});
+
+app.use((err, req, res, next) => {
+	console.error(err.stack)
+	res.status(500).send("Error");
+})
+
+app.listen(PORT, () => {
+	console.log(`Servidor activo http://localhost:${PORT}`);
+});
