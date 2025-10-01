2eb167ba802ca39bf5d9818dd14b7bc840d50741
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-01 20:50:06 +0200

    Ejercicio 1 del dia 17

--- cli-node/03-Express-Rest/day-17-Express-router/Mini-exercises/1-exercise.mjs
+++ cli-node/03-Express-Rest/day-17-Express-router/Mini-exercises/1-exercise.mjs
@@ -4,3 +4,48 @@ Ejercicio 1 (ES):
 Exercise 1 (EN):
 - Create a "users" router with routes GET "/" and GET "/:id".
 */
+
+import express from "express";
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+let users = [
+	{ id: 1, name: "Jose" },
+	{ id: 2, name: "Antonio" },
+	{ id: 3, name: "Manuel" }
+];
+
+app.use((req, res, next) => {
+	console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+	next();
+})
+
+const usersRouter = express.Router();
+
+usersRouter.get("/", (req, res) => {
+	res.json(users);
+});
+
+usersRouter.get("/:id", (req, res) => {
+	const user = users.find(u => u.id === Number(req.params.id));
+	if (!user) {
+		return res.status(404).json({ error: "Usuario no encontrado" });
+	}
+	res.json(user);
+});
+
+app.use("/users", usersRouter);
+
+app.get("/", (req, res) => {
+	res.send("Bienvenido a express.js");
+});
+
+app.use((req, res) => {
+	res.status(404).send("404 - Ruta no encontrada");
+});
+
+app.listen(PORT, () => {
+	console.log(`Servidor activado: http://localhost:${PORT}`);
+});
