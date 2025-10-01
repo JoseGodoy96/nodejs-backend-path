938b34e9de77c5193969c37a84e6e47cef1c5055
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-01 21:52:59 +0200

    Ejercicio 2 del dia 17

--- cli-node/03-Express-Rest/day-17-Express-router/Mini-exercises/2-exercise.mjs
+++ cli-node/03-Express-Rest/day-17-Express-router/Mini-exercises/2-exercise.mjs
@@ -4,3 +4,47 @@ Ejercicio 2 (ES):
 Exercise 2 (EN):
 - Integrate the "users" router into the main server with app.use("/users", userRouter).
 */
+
+import express from "express";
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+let users = [
+	{ id: 1, name: "Jose" },
+	{ id: 2, name: "Jesus" },
+	{ id: 3, name: "Antonio" }
+]
+
+app.use((req, res, next) => {
+	console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+	next();
+})
+
+const usersRouter = express.Router();
+
+usersRouter.get("/", (req, res) => {
+	res.json(users);
+});
+
+usersRouter.get("/:id", (req, res) => {
+	const user = users.find(u => u.id === Number(req.params.id));
+	if (!user)
+		return res.status(404).json({ error: "Usuario no encontrado" });
+	res.json(user);
+});
+
+app.use()
+
+app.get("/", (req, res) => {
+	res.send("Bienvenido a express.js");
+})
+
+app.use((req, res) => {
+	res.status(404).send("No se encontro la ruta");
+})
+
+app.listen(PORT, () => {
+	console.log(`Servidor activado http://localhost:${PORT}`);
+});
