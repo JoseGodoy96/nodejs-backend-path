bdd52462c98f7fdc61a4aa66c29a30716a6c765f
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-02 13:41:39 +0200

    Ejercicio 3 del dia 18

--- cli-node/03-Express-Rest/day-18-Advanced-Validation-&-Error-Handling/Mini-exercises/3-exercises.mjs
+++ cli-node/03-Express-Rest/day-18-Advanced-Validation-&-Error-Handling/Mini-exercises/3-exercises.mjs
@@ -4,3 +4,39 @@ Ejercicio 3 (ES):
 Exercise 3 (EN):
 - Create a validation schema for "products" with {name, price}.
 */
+
+import express from "express";
+import Joi from "joi";
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+	console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+	next();
+});
+
+const productsSchema = Joi.object({
+	name: Joi.string().min(3).required(),
+	price: Joi.number().required()
+});
+
+app.post("/products", (req, res, next) => {
+	const { error } = productsSchema.validate(req.body);
+	if (error)
+		return res.status(400).json({ error: error.details[0].message });
+	res.status(201).json({ message: "Productos creado", data: req.body });
+});
+
+app.get("/", (req, res) => {
+	res.send("BIenvenido a express y Joi");
+});
+
+app.use((req, res) => {
+	res.status(404).send("Ruta desconocida");
+});
+
+app.listen(PORT, () => {
+	console.log(`El servidor esta en activo: http://localhost:${PORT}`);
+});
