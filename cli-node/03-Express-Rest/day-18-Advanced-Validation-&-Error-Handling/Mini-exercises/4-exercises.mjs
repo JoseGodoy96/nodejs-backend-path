2c1db4924182ebb8faf86d46ed79f86d9a7a12cc
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-02 13:11:58 +0200

    Ejercicio 4 del dia 18

--- cli-node/03-Express-Rest/day-18-Advanced-Validation-&-Error-Handling/Mini-exercises/4-exercises.mjs
+++ cli-node/03-Express-Rest/day-18-Advanced-Validation-&-Error-Handling/Mini-exercises/4-exercises.mjs
@@ -4,3 +4,32 @@ Ejercicio 4 (ES):
 Exercise 4 (EN):
 - Implement a global middleware that catches errors and returns status 500.
 */
+
+import express from "express";
+import Joi from "joi";
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("Bienvenido a express y joi");
+});
+
+app.get("/error", (req, res, next) => {
+	const error = new Error("Algo salió mal!");
+	next(error);
+});
+
+app.use((req, res) => {
+	res.status(404).send("ruta no encontrada");
+});
+
+app.use((err, req, res, next) => {
+	console.error(err.message);
+	res.status(500).json( { error: "Error interno de servidor"} );
+});
+
+app.listen(PORT, () => {
+	console.log(`El servidor esta en activo en: http://localhost:${PORT}`);
+});
