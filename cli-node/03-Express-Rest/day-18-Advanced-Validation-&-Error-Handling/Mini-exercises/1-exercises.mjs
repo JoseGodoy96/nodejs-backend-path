ff6ad9c45c32ce0357b2230ff529b427fd823e29
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-02 12:58:55 +0200

    Ejercicio 1 del dia 18

--- cli-node/03-Express-Rest/day-18-Advanced-Validation-&-Error-Handling/Mini-exercises/1-exercises.mjs
+++ cli-node/03-Express-Rest/day-18-Advanced-Validation-&-Error-Handling/Mini-exercises/1-exercises.mjs
@@ -4,3 +4,39 @@ Ejercicio 1 (ES):
 Exercise 1 (EN):
 - Install Joi and create a validation schema for "users" with {name, email}.
 */
+
+import express from "express";
+import Joi from "joi";
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+	console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+	next();
+});
+
+const userSchema = Joi.object({
+	name: Joi.string().min(3).required(),
+	email: Joi.string().email().required()
+});
+
+app.post("/users", (req, res, next) => {
+	const { error } = userSchema.validate(req.body);
+	if (error)
+		return res.status(400).json({ error: error.details[0].message });
+	res.status(201).json({ message: "User created", data: req.body });
+});
+
+app.get("/", (req, res) => {
+	res.send("Bienvenido en express.js");
+});
+
+app.use((req, res) => {
+	res.status(404).send("Ruta no encontrada");
+});
+
+app.listen(PORT, () => {
+	console.log(`El servidor esta activo en: http://localhost:${PORT}`);
+});
