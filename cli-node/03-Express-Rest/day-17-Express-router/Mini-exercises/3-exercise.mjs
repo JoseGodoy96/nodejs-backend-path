84f20dc551d0f3d10134dbee873aeab30a95467d
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-02 09:59:43 +0200

    Ejercicio 3 del dia 17 correccion

--- cli-node/03-Express-Rest/day-17-Express-router/Mini-exercises/3-exercise.mjs
+++ cli-node/03-Express-Rest/day-17-Express-router/Mini-exercises/3-exercise.mjs
@@ -4,3 +4,49 @@ Ejercicio 3 (ES):
 Exercise 3 (EN):
 - Create a "products" router with routes GET "/" and POST "/".
 */
+
+import express from "express";
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+let product = [
+	{ id: 1, name: "naranja" },
+	{ id: 2, name: "limon" },
+	{ id: 3, name: "manzana" }
+];
+
+app.use((req, res, next) => {
+	console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+	next();
+});
+
+const productsRouter = express.Router();
+
+productsRouter.get("/", (req, res) => {
+	res.json(product);
+});
+
+productsRouter.post("/", (req, res) => {
+	const { name } = req.body;
+	if (!name)
+		return res.status(400).json({ error: "Faltan name" });
+	const newProduct = { id: product.length + 1, name };
+	users.push(newProduct);
+	res.status(201).json(newProduct);
+});
+
+app.use("/products", productsRouter);
+
+app.get("/", (req, res) => {
+	res.send("Bienvenido a express.js");
+});
+
+app.use((req, res) => {
+	res.status(404).send("Pagina no encontrada");
+})
+
+app.listen(PORT, () => {
+	console.log(`El sevidor ya esta activo http://localhost:${PORT}`);
+});
