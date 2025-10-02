fb8f7230b2fb1e70b295172b5ae7e0518dd7204e
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-02 11:37:08 +0200

    Minichallenge del dia 17 correccion

--- cli-node/03-Express-Rest/day-17-Express-router/Mini-challenge/minichallenge.mjs
+++ cli-node/03-Express-Rest/day-17-Express-router/Mini-challenge/minichallenge.mjs
@@ -14,3 +14,83 @@ Mini-Challenge (EN):
 	4. Global error handling middleware
 */
 
+import express from "express";
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+let users = [
+	{ id: 1, user: "jose" , password: 1234 },
+	{ id: 2, user: "antonio" , password: 1234 },
+	{ id: 3, user: "jesus" , password: 1234 }
+];
+
+let products = [
+	{ id: 1, name: "manzana" },
+	{ id: 2, name: "pera" },
+	{ id: 3, name: "limon" }
+];
+
+app.use((req, res, next) => {
+	console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+	next();
+});
+
+const routerUsers = express.Router();
+
+routerUsers.get("/", (req, res) => {
+	res.json(users);
+});
+
+routerUsers.post("/", (req, res) => {
+	const { user, password } = req.body;
+	if (!user || !password)
+		return res.status(400).json({error: "user o password incorrecto"});
+	const newUser = { id: users.length + 1, user, password };
+	users.push(newUser);
+	res.status(201).json(newUser);
+});
+
+routerUsers.get("/:id", (req, res) => {
+	const idUser = users.find(u => u.id === Number(req.params.id));
+	if (!idUser)
+		return res.status(404).json({ error: "id incorrecto" });
+	res.json(idUser);
+});
+
+app.use("/users", routerUsers);
+
+const routesProducts = express.Router();
+
+routesProducts.get("/", (req, res) => {
+	res.json(products);
+});
+
+routesProducts.post("/", (req, res) => {
+	const { name } = req.body;
+	if (!name)
+		return res.status(400).json({error: "name incorrecto"});
+	const newProduct = { id: products.length + 1, name };
+	products.push(newProduct);
+	res.status(201).json(newProduct);
+})
+
+app.use("/products", routesProducts);
+
+app.get("/", (req, res) => {
+	res.send("Bienvenido a express.js");
+});
+
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(err.status || 500).json({ error: err.message || "Error interno del servidor" });
+});
+
+app.use((req, res) => {
+	res.status(404).send("Ruta no encontrada");
+});
+
+app.listen(PORT, () => {
+	console.log(`El servidor ya esta activo http://localhost:${PORT}`);
+});
--- cli-node/03-Express-Rest/day-17-Express-router/Mini-exercises/4-exercise.mjs
+++ cli-node/03-Express-Rest/day-17-Express-router/Mini-exercises/4-exercise.mjs
@@ -4,3 +4,47 @@ Ejercicio 4 (ES):
 Exercise 4 (EN):
 - Integrate the "products" router into the main server with app.use("/products", productRouter).
 */
+
+import express, { json } from "express";
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+let products = [
+	{ id: 1, name: "manzana" },
+	{ id: 2, name: "pera" },
+	{ id: 3, name: "limon" }
+];
+
+app.use((req, res, next) => {
+	console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+	next();
+})
+
+const routesProducts = express.Router();
+
+routesProducts.get("/", (req, res) => {
+	res.json(products);
+});
+
+routesProducts.get("/:id", (req, res) => {
+	const product = products.find(u => u.id === Number(req.params.id));
+	if (!product)
+		return res.status(404).json({err: "no existe ese producto"});
+	res.json(product);
+});
+
+app.use("/products", routesProducts);
+
+app.get("/", (req, res) => {
+	res.send("Bienvenido a express.js");
+});
+
+app.use((req, res) => {
+	res.status(404).send("No se encuentra la pagina");
+});
+
+app.listen(PORT, () => {
+	console.log(`El servidor esta abierto en: http://localhost:${PORT}`);
+});
