b8a5af2d79afcce3b50256b66f911c91ab572989
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-03 20:02:00 +0200

    ejercicio dia 1 dia 20 aun no finalizado

--- cli-node/03-Express-Rest/day-20-Mini-Project:JWT-API/Mini-exercise/1-exercises.mjs
+++ cli-node/03-Express-Rest/day-20-Mini-Project:JWT-API/Mini-exercise/1-exercises.mjs
@@ -4,3 +4,53 @@ Ejercicio 1 (ES):
 Exercise 1 (EN):
 - Create a "users" router with POST "/signup" to register users (use bcrypt).
 */
+
+import express from "express";
+import Joi from "joi";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const app = express();
+const PORT = 3000;
+const JWT_SECRET = "mi_contraseña_correcta";
+
+app.use(express.json());
+
+let users = [
+	{user: "jose", password: bcrypt.hashSync("1234", 10)}
+];
+
+app.use((req, res, next) => {
+	console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+	next();
+});
+
+const usersScheme = Joi.object({
+	user: Joi.string().required(),
+	password: Joi.string().required()
+});
+
+const usersRouter = express.Router();
+
+usersRouter.post("/signup", async (req, res) => {
+	
+});
+
+app.use("/users", usersRouter);
+
+app.get("/", (req, res) => {
+	res.send("Bienvenido a express, joi, jwt y bcrypt");
+});
+
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).json({error: "Error interno del servidor"});
+});
+
+app.use((req, res) => {
+	res.status(404).send("Ruta desconocida");
+});
+
+app.listen(PORT, () => {
+	console.log(`El servidor esta activo en: http://localhost:${PORT}`);
+});
