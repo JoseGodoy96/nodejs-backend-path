6c92988c321f185bb9886fc5392e591b5181d36f
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-06 14:08:54 +0200

    Ejercicio 6 del dia 21 preparado

--- cli-node/04-SQL-NoSQL/day-21-Introduction-to-SQL-&-PostgreSQL/Mini-exercise/6-exercise.sql
+++ cli-node/04-SQL-NoSQL/day-21-Introduction-to-SQL-&-PostgreSQL/Mini-exercise/6-exercise.sql
@@ -4,3 +4,22 @@ Ejercicio 6 (ES):
 Exercise 6 (EN):
 - Delete a specific user.
 */
+
+CREATE DATABASE blogdb;
+
+\c blogdb;
+
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	user VARCHAR(50) UNIQUE NOT NULL,
+	email VARCHAR(100) UNIQUE NOT NULL,
+	password VARCHAR(200) NOT NULL
+);
+
+INSERT INTO users (user, email, password)
+VALUES
+	('jose', 'jose@example.com', '1234'),
+	('jesus', 'jesus@example.com', '1234'),
+	('antonio', 'antonio@example.com', '1234');
+
+DELETE FROM users WHERE id = 2;
