2665c8d8122367415f04f3a823d0f23d82178a15
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-06 19:06:27 +0200

    minichallenge dia 22 preparado

--- cli-node/04-SQL-NoSQL/day-22-Mongo-basics/Mini-challenge/minichallenge.mjs
+++ cli-node/04-SQL-NoSQL/day-22-Mongo-basics/Mini-challenge/minichallenge.mjs
@@ -17,3 +17,29 @@ Mini-Challenge (EN):
 	3. Update a comment.
 	4. Delete a user.
 */
+
+const database = 'blogDB';
+use(database);
+
+const ana = db.users.insertOne({ username: "Ana", email: "ana@example.com", password: "1234" });
+const maria = db.users.insertOne({ username: "Maria", email: "maria@example.com", password: "1234" });
+
+const post1 = db.posts.insertOne({ user_id: ana.insertedId, title: "Primer post", content: "Hola mundo!" });
+db.posts.insertOne({ user_id: maria.insertedId, title: "Segundo post", content: "Mi segundo post" });
+
+db.comments.insertMany([
+	{ post_id: post1.insertedId, user_id: ana.insertedId, comment_text: "Buen post!" },
+	{ post_id: post1.insertedId, user_id: maria.insertedId, comment_text: "Gracias por comentar" }
+]);
+
+db.posts.find();
+db.comments.find({ post_id: post1.insertedId }).pretty();
+
+db.comments.updateOne(
+	{ user_id: ana.insertedId },
+	{ $set: { comment_text : "nuevo comentario"}}
+);
+
+db.users.deleteOne({ username: "Ana" });
+db.comments.deleteMany({ user_id: ana.insertedId });
+db.posts.deleteMany({ user_id: ana.insertedId });
