3af0074fe9fb870647be0df4d362128ef6009c75
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-06 13:57:13 +0200

    Ejercicio 4 del dia 21 preparado

--- cli-node/04-SQL-NoSQL/day-21-Introduction-to-SQL-&-PostgreSQL/Mini-exercise/4-exercise.sql
+++ cli-node/04-SQL-NoSQL/day-21-Introduction-to-SQL-&-PostgreSQL/Mini-exercise/4-exercise.sql
@@ -4,3 +4,22 @@ Ejercicio 4 (ES):
 Exercise 4 (EN):
 - Perform SELECT queries to get all users.
 */
+
+CREATE DATABASE blogdb;
+
+\c blogdb;
+
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	user VARCHAR(50) UNIQUE NOT NULL,
+	email VARCHAR(100) UNIQUE NOT NULL,
+	password VARCHAR(200) NOT NULL
+);
+
+INSERT INTO users (user, email, password)
+VALUES 
+	('Jose', 'Jose@example.com', '1234'),
+	('J', 'J@example.com', '1234'),
+	('Jo', 'Jo@example.com', '1234');
+
+SELECT * FROM users;
