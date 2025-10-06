240249826da56e925f372eb6823f628122426e67
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-06 14:36:23 +0200

    Minichallenge del dia 21 preparado

--- cli-node/04-SQL-NoSQL/day-21-Introduction-to-SQL-&-PostgreSQL/Mini-challenge/minichallenge.sql
+++ cli-node/04-SQL-NoSQL/day-21-Introduction-to-SQL-&-PostgreSQL/Mini-challenge/minichallenge.sql
@@ -13,3 +13,41 @@ Mini-Challenge (EN):
 	1. List all posts with the author's name.
 	2. List all comments of a specific post.
 */
+
+CREATE DATABASE blogdb;
+
+\c blogdb;
+
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	user VARCHAR(50) UNIQUE NOT NULL,
+	email VARCHAR(100) UNIQUE NOT NULL,
+	password VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE posts (
+	post_id SERIAL PRIMARY KEY,
+	user_id INT NOT NULL,
+	title VARCHAR(200) NOT NULL,
+	content TEXT NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE comments (
+    comment_id SERIAL PRIMARY KEY,
+    post_id INT NOT NULL,
+    user_id INT NOT NULL,
+    comment_text TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (post_id) REFERENCES posts(post_id),
+    FOREIGN KEY (user_id) REFERENCES users(id)    
+);
+
+SELECT posts.title, users.user
+FROM posts
+INNER JOIN users ON posts.user_id = users.id;
+
+SELECT comments.comment_text, users.user
+FROM comments
+INNER JOIN users ON comments.user_id = users.id
+WHERE comments.post_id = 1;
