04bda29de6dee955f3db5d8473d27f47f02ac127
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-06 13:50:57 +0200

    Ejercicio 3 del dia 21 preparado

--- cli-node/04-SQL-NoSQL/day-21-Introduction-to-SQL-&-PostgreSQL/Mini-exercise/3-exercise.sql
+++ cli-node/04-SQL-NoSQL/day-21-Introduction-to-SQL-&-PostgreSQL/Mini-exercise/3-exercise.sql
@@ -4,3 +4,18 @@ Ejercicio 3 (ES):
 Exercise 3 (EN):
 - Insert 3 example users.
 */
+
+CREATE DATABASE blogdb;
+
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	username VARCHAR(50) UNIQUE NOT NULL,
+	email VARCHAR(100) UNIQUE NOT NULL,
+	password VARCHAR(250) NOT NULL
+);
+
+INSERT INTO users (username, email, password)
+VALUES 
+	('Jose', 'Jose@example.com', '1234'),
+	('Jesus', 'Jesus@example.com', '1234'),
+	('Antonio', 'Antonio@example.com', '1234');
