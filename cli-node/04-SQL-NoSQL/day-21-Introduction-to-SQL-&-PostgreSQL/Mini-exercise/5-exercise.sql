aade83309ccc35f639b52fe27a15748a3c14fda8
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-06 14:02:45 +0200

    Ejercicio 5 del dia 21 preparado

--- cli-node/04-SQL-NoSQL/day-21-Introduction-to-SQL-&-PostgreSQL/Mini-exercise/5-exercise.sql
+++ cli-node/04-SQL-NoSQL/day-21-Introduction-to-SQL-&-PostgreSQL/Mini-exercise/5-exercise.sql
@@ -4,3 +4,22 @@ Ejercicio 5 (ES):
 Exercise 5 (EN):
 - Update the email of a specific user.
 */
+
+CREATE DATABASE blogdb;
+
+\c blogdb;
+
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	user VARCHAR(50) UNIQUE NOT NULL,
+	email VARCHAR(100) UNIQUE NOT NULL,
+	password VARCHAR(200) NOT NULL
+);
+
+INSERT INTO users (user, email, password)
+VALUES
+	('jose', 'jose@example.com', '1234'),
+	('jesus', 'jesus@example.com', '1234'),
+	('antonio', 'antonio@example.com', '1234');
+
+UPDATE users SET email = 'new@example.com' WHERE id = 2;
