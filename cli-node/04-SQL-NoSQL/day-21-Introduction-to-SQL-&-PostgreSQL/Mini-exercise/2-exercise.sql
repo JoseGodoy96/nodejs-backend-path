1f10886a01461c65723f7ea72315b2cd3d7bd4c2
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-06 13:41:36 +0200

    Ejercicio 2 del dia 21 preparado

--- cli-node/04-SQL-NoSQL/day-21-Introduction-to-SQL-&-PostgreSQL/Mini-exercise/2-exercise.sql
+++ cli-node/04-SQL-NoSQL/day-21-Introduction-to-SQL-&-PostgreSQL/Mini-exercise/2-exercise.sql
@@ -4,3 +4,12 @@ Ejercicio 2 (ES):
 Exercise 2 (EN):
 - Create a "users" table with columns: id, username, email, password.
 */
+
+CREATE DATABASE blogdb;
+
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	username VARCHAR(50) UNIQUE NOT NULL,
+	email VARCHAR(100) UNIQUE NOT NULL,
+	password VARCHAR(250) NOT NULL
+);
