51ea45316d51b44f9d1d1b361b90043ee9133003
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-08 13:29:26 +0200

    ejercicio 6 del dia 23

--- cli-node/04-SQL-NoSQL/day-23-Node.js-PostgreSQL/Mini-exercise/6-exercise.mjs
+++ cli-node/04-SQL-NoSQL/day-23-Node.js-PostgreSQL/Mini-exercise/6-exercise.mjs
@@ -4,3 +4,33 @@ Ejercicio 6 (ES):
 Exercise 6 (EN):
 - Create a function to delete a user by id.
 */
+
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const pool = new Pool({
+	user: 'postgres',
+	host: 'localhost',
+	database: 'blogdb',
+	password: "mi_contraseña",
+	port: 5432,
+});
+
+async function delUser(id) {
+	try {
+		let res;
+		if (!id) {
+			console.log("ID necesario");
+		} else {
+		const query = ('DELETE FROM users WHERE id = $1;')
+		res = await pool.query(query, [id]);
+		console.log(`Usuario borrado: ${res.rowCount}`);
+		}
+	} catch (err) {
+		console.error(err);
+	} finally {
+		await pool.end();
+	}
+};
+
+delUser(4);
