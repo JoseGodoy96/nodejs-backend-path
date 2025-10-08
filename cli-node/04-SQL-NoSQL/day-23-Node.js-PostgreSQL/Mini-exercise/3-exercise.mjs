a9781bd18928efb0073b351954fb4e126f255aae
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-08 11:49:53 +0200

    ejercicio 3 del dia 23

--- cli-node/04-SQL-NoSQL/day-23-Node.js-PostgreSQL/Mini-exercise/3-exercise.mjs
+++ cli-node/04-SQL-NoSQL/day-23-Node.js-PostgreSQL/Mini-exercise/3-exercise.mjs
@@ -4,3 +4,29 @@ Ejercicio 3 (ES):
 Exercise 3 (EN):
 - Create a function to insert a user into the "users" table.
 */
+
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const pool = new Pool({
+	user: 'postgres',
+	host: 'localhost',
+	database: 'blogdb',
+	password: 'mi_contraseña',
+	port: 5432,
+});
+
+async function insertUser() {
+	try {
+		const text = 'INSERT INTO users (name, email) VALUES ($1, $2);';
+		const values = ['Juan', 'juan@example.com'];
+		const res = await pool.query(text, values);
+		console.log("Usuario insertado correctamente");
+	} catch (err) {
+		console.error(err);
+	} finally {
+		await pool.end();
+	}
+};
+
+insertUser();
