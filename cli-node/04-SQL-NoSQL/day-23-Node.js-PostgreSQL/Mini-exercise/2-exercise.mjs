db5656a1777169603ff4ee38ea296c89572f84f6
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-08 11:25:31 +0200

    ejercicio 2 del dia 23 y creacion de la base de datos

--- cli-node/04-SQL-NoSQL/day-23-Node.js-PostgreSQL/Mini-exercise/2-exercise.mjs
+++ cli-node/04-SQL-NoSQL/day-23-Node.js-PostgreSQL/Mini-exercise/2-exercise.mjs
@@ -4,3 +4,25 @@ Ejercicio 2 (ES):
 Exercise 2 (EN):
 - Connect Node.js to PostgreSQL using 'pg'.
 */
+
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const pool = new Pool({
+	user: 'postgres',
+	host: 'localhost',
+	database: 'blogdb',
+	password: 'mi_contraseña',
+	port: 5432,
+});
+
+async function getUser() {
+	try {
+		const res = await pool.query('SELECT * FROM users');
+		console.log(res.rows);
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+getUser();
