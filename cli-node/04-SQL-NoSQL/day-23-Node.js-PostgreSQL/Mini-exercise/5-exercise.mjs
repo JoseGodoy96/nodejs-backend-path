af15870924df268df18b06997a890b228d9b9a4e
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-08 12:34:02 +0200

    ejercicio 5 del dia 23

--- cli-node/04-SQL-NoSQL/day-23-Node.js-PostgreSQL/Mini-exercise/5-exercise.mjs
+++ cli-node/04-SQL-NoSQL/day-23-Node.js-PostgreSQL/Mini-exercise/5-exercise.mjs
@@ -4,3 +4,33 @@ Ejercicio 5 (ES):
 Exercise 5 (EN):
 - Create a function to update a specific user's email.
 */
+
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const pool = new Pool({
+	user: 'postgres',
+	host: 'localhost',
+	database: 'blogdb',
+	password: 'mi_contraseña',
+	port: 5432,
+});
+
+async function newEmail(email, id) {
+	try {
+		let res;
+		if (!email || !id) {
+			console.log("Es necesario Email e ID");
+		} else {
+			const query = 'UPDATE users SET email = $1 WHERE id = $2;';
+			res = await pool.query(query, [email, id]);
+			console.log(`Filas actualizadas: ${res.rowCount}`);
+		}
+	} catch (err) {
+		console.error(err);
+	} finally {
+		await pool.end();
+	}
+};
+
+newEmail('new@email.com', 1);
