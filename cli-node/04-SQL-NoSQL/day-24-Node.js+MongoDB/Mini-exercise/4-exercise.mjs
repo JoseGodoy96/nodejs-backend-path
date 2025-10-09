f27e495c3deba32ac36ca6d324f49c490556c508
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-09 13:46:01 +0200

    Exercise 4 dia 24

--- cli-node/04-SQL-NoSQL/day-24-Node.js+MongoDB/Mini-exercise/4-exercise.mjs
+++ cli-node/04-SQL-NoSQL/day-24-Node.js+MongoDB/Mini-exercise/4-exercise.mjs
@@ -4,3 +4,32 @@ Ejercicio 4 (ES):
 Exercise 4 (EN):
 - Update the email of a specific user.
 */
+
+import { MongoClient } from "mongodb";
+
+const username = encodeURIComponent("josemariagodoy1996_db_user");
+const password = encodeURIComponent("micontraseña1234");
+
+const uri = `mongodb+srv://${username}:${password}@cluster0.is9fdnf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri);
+
+async function run() {
+	try {
+		await client.connect();
+		console.log("Conectado a MongoDB");
+		const db = client.db('blogDB');
+		const collection = db.collection('users');
+		const update = await collection.updateOne(
+			{username: "Ana"},
+			{ $set: {email: "newemail@example.com"}}
+		);
+		const users = await collection.find().toArray();
+		console.log(users);
+	} catch (err) {
+		console.error(err);
+	} finally {
+		await client.close();
+	}
+}
+
+run();
