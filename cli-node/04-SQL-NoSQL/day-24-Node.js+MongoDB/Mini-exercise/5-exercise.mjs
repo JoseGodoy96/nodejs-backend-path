c9d97eaccb47fef339393ad5f90b0ca203a87658
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-09 13:57:14 +0200

    Exercise 5 dia 24

--- cli-node/04-SQL-NoSQL/day-24-Node.js+MongoDB/Mini-exercise/5-exercise.mjs
+++ cli-node/04-SQL-NoSQL/day-24-Node.js+MongoDB/Mini-exercise/5-exercise.mjs
@@ -5,3 +5,28 @@ Exercise 5 (EN):
 - Delete a user by username.
 */
 
+import { MongoClient } from "mongodb";
+
+const username = encodeURIComponent("josemariagodoy1996_db_user");
+const password = encodeURIComponent("micontraseña1234");
+
+const uri = `mongodb+srv://${username}:${password}@cluster0.is9fdnf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri);
+
+async function run() {
+	try {
+		await client.connect();
+		console.log("Conectado a MongoDB");
+		const db = client.db('blogdb');
+		const collection = db.collection('users');
+		const result = await collection.deleteOne({username: "Jose"});
+		const users = await collection.find().toArray();
+		console.log(users);
+	} catch (err) {
+		console.error(err);
+	} finally {
+		await client.close();
+	}
+}
+
+run();
