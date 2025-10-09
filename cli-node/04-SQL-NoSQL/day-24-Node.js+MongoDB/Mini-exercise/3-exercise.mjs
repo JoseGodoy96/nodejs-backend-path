65e736bb53a2dcf5ef95e5b82fc14316d01b3bef
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-09 13:38:31 +0200

    Exercise 3 dia 24

--- cli-node/04-SQL-NoSQL/day-24-Node.js+MongoDB/Mini-exercise/3-exercise.mjs
+++ cli-node/04-SQL-NoSQL/day-24-Node.js+MongoDB/Mini-exercise/3-exercise.mjs
@@ -4,3 +4,28 @@ Ejercicio 3 (ES):
 Exercise 3 (EN):
 - Read all users and print them to the console.
 */
+
+import { MongoClient } from "mongodb";
+
+const username = encodeURIComponent("josemariagodoy1996_db_user");
+const password = encodeURIComponent("micontraseña1234");
+
+const uri = `mongodb+srv://${username}:${password}@cluster0.is9fdnf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri);
+
+async function run() {
+	try {
+		await client.connect();
+		console.log("Conectado a MongoDB");
+		const db = client.db('blogDB');
+		const collection = db.collection('users');
+		const users = await collection.find().toArray();
+		console.log(users);
+	} catch (err) {
+		console.error(err);
+	} finally {
+		await client.close();
+	}
+}
+
+run();
