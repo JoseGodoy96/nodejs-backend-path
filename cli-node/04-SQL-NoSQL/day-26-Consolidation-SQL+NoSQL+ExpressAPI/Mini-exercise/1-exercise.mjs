a489910e2599c5f0dc6bb025f4e772be81c0f889
Jose Maria <josemariagodoy1996@gmail.com>
2025-10-12 14:44:48 +0200

    ejercicio 1 dia 26

--- cli-node/04-SQL-NoSQL/day-26-Consolidation-SQL+NoSQL+ExpressAPI/Mini-exercise/1-exercise.mjs
+++ cli-node/04-SQL-NoSQL/day-26-Consolidation-SQL+NoSQL+ExpressAPI/Mini-exercise/1-exercise.mjs
@@ -4,3 +4,47 @@ Ejercicio 1 (ES):
 Exercise 1 (EN):
 - Create an Express project handling routes for "users" and "posts".
 */
+
+import express from 'express';
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+	console.log(`${new Date().toISOString()} ${req.method} ${req.url}`);
+	next();
+})
+
+app.get("/", (req, res) => {
+	res.send("Bienvenido a node.js y express");
+});
+
+const routerUsers = express.Router();
+
+routerUsers.get("/", (req, res) => {
+	res.send("Ruta de users");
+});
+
+app.use("/users", routerUsers);
+
+const routerPosts = express.Router();
+
+routerPosts.get("/", (req, res) => {
+	res.send("Ruta de posts");
+});
+
+app.use("/posts", routerPosts);
+
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).json({error: "error interno de servidor"});
+});
+
+app.use((req, res) => {
+	res.status(404).send("Ruta desconocida");
+});
+
+app.listen(PORT, () => {
+	console.log(`El servidor esta activo en http://localhost:${PORT}`);
+});
